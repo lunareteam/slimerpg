b18b4287ea9be77761f5dd7930d5ed45e5944692
OakBranches <matheusramosimperador@gmail.com>
2019-10-22 19:09:34 -0300

    Movement changes

---
+++ Assets/Scripts/cam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject player;
+
+
+    Vector3 objPosition;
+    void Start()
+    {
+
+    }
+    
+    // Update is called once per fram
+    void Update()
+    {
+        Vector2 ob= new Vector2(player.transform.position.x, player.transform.position.y);
+        this.transform.position = ob;
+    
+            Debug.Log("slime :" + (player.transform.position.x.ToString())+" "+(player.transform.position.y.ToString())+" "+ player.transform.position.z.ToString());
+        Debug.Log("Cam :" + (this.transform.position.x.ToString()) + " " + (this.transform.position.y.ToString()) + " " + this.transform.position.z.ToString());
+    }
+}
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject player;
+    [SerializeField] float vel;
+    [SerializeField] Camera mapacamera;
+
+    Vector3 objPosition ;
+    void Start()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        Debug.Log("socorr");
+        Vector2 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y,10);
+        objPosition = new Vector3(mapacamera.ScreenToWorldPoint(mousePosition).x, mapacamera.ScreenToWorldPoint(mousePosition).y, 10);
+        
+    }
+    // Update is called once per fram
+    void Update()
+    {
+        player.transform.position = Vector3.MoveTowards(player.transform.position, objPosition, vel);
+    }
+}
