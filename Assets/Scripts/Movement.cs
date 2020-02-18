1f09b57baf5d8fe2c882cc48de4c2be55cecae48
João Bueno <joaovictorbuenodecaldas@gmail.com>
2020-02-18 11:51:22 -0300

    Added movement on Z axis.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,6 +8,8 @@ public class Movement : MonoBehaviour
     [SerializeField] float vel;
     public LayerMask bgLayer;
     Vector3 mousePosition;
+    Vector3 trueMousePosition;
+    float mousePositionZ;
     bool clickedOnce = false;
 
     // Start is called before the first frame update
@@ -22,12 +24,16 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0) || clickedOnce)
         {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-            Debug.Log("Click on " + hit.collider.tag);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+                Debug.Log("Click on " + hit.collider.tag);
             
             if (hit.collider.tag == "Background")
             {
-                mousePosition = hit.point;
+                trueMousePosition = hit.point;
+                mousePosition = new Vector3(hit.point.x, hit.point.y, player.GetComponent<Transform>().position.z);
                 clickedOnce = true;
             }
             else
@@ -42,12 +48,15 @@ public class Movement : MonoBehaviour
         }
 
         player.GetComponent<Transform>().position = Vector3.MoveTowards(player.GetComponent<Transform>().position, mousePosition, vel);
+        Vector3 newPos = new Vector3(player.GetComponent<Transform>().position.x, player.GetComponent<Transform>().position.y, trueMousePosition.z - 20);
+        player.GetComponent<Transform>().position = Vector3.MoveTowards(player.GetComponent<Transform>().position, newPos, vel);
         if (player.GetComponent<Transform>().position.z != -1 && clickedOnce)
         {            
-            player.GetComponent<Transform>().position = new Vector3(player.GetComponent<Transform>().position.x, player.GetComponent<Transform>().position.y, 1);
+            player.GetComponent<Transform>().position = new Vector3(player.GetComponent<Transform>().position.x, player.GetComponent<Transform>().position.y, player.GetComponent<Transform>().position.z);
+            player.GetComponent<Transform>().position = new Vector3(player.GetComponent<Transform>().position.x, player.GetComponent<Transform>().position.y, trueMousePosition.z - 20);
         }
 
-        if(player.GetComponent<Transform>().position != mousePosition)
+        if(player.GetComponent<Transform>().position != new Vector3(trueMousePosition.x, trueMousePosition.y, trueMousePosition.z - 20))
         {
             player.GetComponent<Animator>().SetBool("move", true);
         }
